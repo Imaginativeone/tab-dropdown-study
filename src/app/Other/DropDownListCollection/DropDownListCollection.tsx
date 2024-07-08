db8d0fb27136cc38a7e0641c65756f58aea871b5
Doug Franklin <dfranklin@copt.com>
2024-07-08 15:17:29 -0400

    Question Ready

--- src/app/Other/DropDownListCollection/DropDownListCollection.tsx
+++ src/app/Other/DropDownListCollection/DropDownListCollection.tsx
@@ -1,3 +1,5 @@
+import { DropDownList } from "@progress/kendo-react-dropdowns"
+
 const DropDownListCollection = (props:any) => {
 
   const { selectedTab, controls } = props
@@ -6,7 +8,7 @@ const DropDownListCollection = (props:any) => {
 
   return (
     <>
-      <h3>DropdownListCollection</h3>
+      {/* <h3>DropdownListCollection</h3> */}
       <Controls controls={controls} selectedTab={selectedTab} />
     </>
   )
@@ -16,13 +18,34 @@ export default DropDownListCollection
 
 function Controls({ controls, selectedTab}) {
 
-  const isControl = Array.isArray(controls.gridControls)
+  const isControl = Array.isArray(controls.gridControls); // console.log('isControl', isControl)
 
-  console.log('isControl', isControl)
+  const handleDropdownSelection = (event:any) => {
+    console.log('event', event.value)
+    console.log('controls', controls)
+  }
 
   return (
     <>
-      <h3>Controls (Component)</h3>
+      {/* <h3>Controls (Component)</h3> */}
+      {
+        controls.gridControls.map((controlElement:any, index:number) => {
+          // console.log('controlElement', controlElement)
+
+          return (
+            <div key={index}>
+              {index}
+              <DropDownList 
+                data={controlElement.apiDropdownControls} 
+                textField="text" 
+                dataItemKey="id" 
+                defaultValue={controlElement.apiDropdownDefaultValue}
+                onChange={handleDropdownSelection}
+              />
+            </div>
+          )
+        })
+      }
     </>
   )
   
