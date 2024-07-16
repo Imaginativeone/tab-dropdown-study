a94595a202dd883e93aa5cc21b897bb88a633f8e
Doug Franklin <dfranklin@copt.com>
2024-07-16 12:18:14 -0400

    Filters updated

--- src/app/Other/DropDownListCollection/controls-constants/getCommissionDataSet/controls/controls.ts
+++ src/app/Other/DropDownListCollection/controls-constants/getCommissionDataSet/controls/controls.ts
@@ -5,10 +5,10 @@ const commissionControls = {
     {
       apiDropdownName: 'Commission Controls',
       apiDropdownLabel: "Label Commission",
-      apiDropdownDefaultValue: { id: 1, text: 'Comm - Yes', filter: null, defaultValue: true },
+      apiDropdownDefaultValue: { id: 1, text: 'Comm - Yes', filter: "filterYes", defaultValue: true },
       apiDropdownControls: [
-        { id: 1, text: 'Comm - Yes', filter: null, defaultValue: true },
-        { id: 2, text: 'Comm - No', filter: null },
+        { id: 1, text: 'Comm - Yes', filter: "filterYes", defaultValue: true },
+        { id: 2, text: 'Comm - No', filter: "filterNo" },
         { id: 3, text: 'Comm - Yes/No', filter: null },
       ]
     }
