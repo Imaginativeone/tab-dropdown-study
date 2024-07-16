3ece78b977c8aff092f7783766c3f89c960e9d26
Doug Franklin <dfranklin@copt.com>
2024-07-16 10:04:18 -0400

    Problem: DropdownListCollection.tsx, line 41 gets hit 3 times. I'd like for it to be twice instead.

--- src/app/Other/DropDownListCollection/controls-constants/getRofoDataSet/controls/controls.ts
+++ src/app/Other/DropDownListCollection/controls-constants/getRofoDataSet/controls/controls.ts
@@ -7,23 +7,24 @@ const rofoControls = {
   [
     {
       apiDropdownName: "rofo - A",
-      apiDropdownDefaultValue: { id: 1, text: 'Rofo - Yes',    filter: rofo_rofr_filter_terms[0], defaultValue:true },
+      apiDropdownDefaultValue: { id: 2, text: 'Rofo - No', filter: rofo_rofr_filter_terms[1] },
       apiDropdownLabel: "Label A",
       apiDropdownControls: [
-        { id: 1, text: 'Rofo - Yes',    filter: rofo_rofr_filter_terms[0], defaultValue:true },
-        { id: 2, text: 'Rofo - No',     filter: rofo_rofr_filter_terms[1] },
+        { id: 1, text: 'Rofo - Yes',    filter: rofo_rofr_filter_terms[0] },
+        { id: 2, text: 'Rofo - No',     filter: rofo_rofr_filter_terms[1], defaultValue:true },
         { id: 3, text: 'Rofo - Yes/No', filter: rofo_rofr_filter_terms[2] },
       ]
     },
-    // {
-    //   apiDropdownName: "rofo - B",
-    //   apiDropdownLabel: "Label B",
-    //   apiDropdownControls: [
-    //     { id: 1, text: 'A', filter: null },
-    //     { id: 2, text: 'B', filter: null, defaultValue: true },
-    //     { id: 3, text: 'C', filter: null },
-    //   ]
-    // },
+    {
+      apiDropdownName: "rofo - B",
+      apiDropdownDefaultValue: { id: 2, text: 'B', filter: null, defaultValue: true },
+      apiDropdownLabel: "Label B",
+      apiDropdownControls: [
+        { id: 1, text: 'A', filter: null },
+        { id: 2, text: 'B', filter: null, defaultValue: true },
+        { id: 3, text: 'C', filter: null },
+      ]
+    },
   ]
 }
 
